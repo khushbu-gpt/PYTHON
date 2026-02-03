7ab25c28347fb94526acffc185adc608f568cdc6
khushbu kumari <khushbugpt212005@gmail.com>
2026-02-03 23:04:09 +0530

    practice ifelse

--- ifElse.py
+++ ifElse.py
@@ -11,3 +11,20 @@ elif num==0:
     print("Zero")
 else:
     print("Negative ")
+
+has_ticket=True
+
+if age >= 18 and has_ticket:
+    print("Allowed")
+else:
+    print("Not Allowed")
+
+x = 5
+
+if x:
+    if x > 10:
+        print("A")
+    else:
+        print("B")
+else:
+    print("C")
