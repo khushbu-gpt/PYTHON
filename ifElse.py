1a206176307329b231b7927058bfc92077d56891
khushbu kumari <khushbugpt212005@gmail.com>
2026-02-03 22:13:08 +0530

    Add age classification and number evaluation functionality

---
+++ ifElse.py
@@ -0,0 +1,13 @@
+age=18
+if age >=18 :
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+num=int(input("Enter a number"))
+if(num>0):
+    print("Positive")
+elif num==0:
+    print("Zero")
+else:
+    print("Negative ")
