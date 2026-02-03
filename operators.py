b3e98193063d11b1d36b6c07e8a3c3ebb89e8b9d
khushbu kumari <khushbugpt212005@gmail.com>
2026-02-03 23:04:44 +0530

    operators types

---
+++ operators.py
@@ -0,0 +1,6 @@
+# Arithmetic Operators
+# Comparison Operators
+# Logical Operators  => and   or   not
+# Assignment Operators
+# Membership Operators=>in   not in
+# Identity Operators=> is   is not
