6904219037dc897f3300b820927ce5de8c432cf0
khushbu kumari <khushbugpt212005@gmail.com>
2026-02-03 17:30:42 +0530

    Add initial variable handling and user input functionality

---
+++ variable.py
@@ -0,0 +1,8 @@
+name="khushbu"
+age=20
+print(name,age)
+username=input("Enter your name:")
+userage=int(input("What is your age:"))
+print("Your name is",username)
+print("Your age is",userage+1)
+print(f"Hello {username} you are {userage} years old")
